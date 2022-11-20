aa4bee7c8f9a2928a5f98c1c7b8eed380ff33077
Dixiekun <dixiekun.pacheco@gmail.com>
2022-11-20 14:25:39 +0800

    added nav buttons in project pages

--- src/routes/[projectSlug]/ProjectNavigator.svelte
+++ src/routes/[projectSlug]/ProjectNavigator.svelte
@@ -44,10 +44,19 @@ $: ({prevIndex, nextIndex } = getPrevAndNext())
     <div class="container">
         <div class="wrapper">
             {#if prevIndex}
-                <a class="button-navigator" href={prevIndex.slug} data-sveltekit-prefetch>Previous Project</a>
+                <a class="button-navigator" href={prevIndex.slug} data-sveltekit-prefetch>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.75 19.5L8.25 12L15.75 4.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    Previous Project
+                </a>
             {/if}
             {#if nextIndex}
-                <a class="button-navigator" href={nextIndex.slug} data-sveltekit-prefetch>Next Project</a>
+                <a class="button-navigator" href={nextIndex.slug} data-sveltekit-prefetch>Next Project
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.25 4.5L14.75 12L7.25 19.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </a>
             {/if}
         </div>
     </div>
